a3d0aca05da215aa59cbb0707c4f1d18faeef629
jhyago <jaquesoantunes@gmail.com>
2024-05-06 17:04:13 -0300

    exercise 4 and 5

---
+++ algorithms/5-diagonalDifference.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function diagonalDifference(arr) {
+    let sumL = 0;
+    let sumR = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sumL += arr[i][i];
+        sumR += arr[i][arr.length - 1 - i];
+    }
+
+    return Math.abs(sumL - sumR);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
